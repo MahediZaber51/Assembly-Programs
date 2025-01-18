494e41ca4b3fb092278b976d488a59925133a3d0
Mahedi Zaman Zaber <88625958+MahediZaber51@users.noreply.github.com>
2025-01-18 21:09:32 +0600

    Create Uppercase_to_Lowercase.asm

---
+++ Case Convertion/Uppercase_to_Lowercase.asm
@@ -0,0 +1,44 @@
+; Data Segment
+.Data
+temp1 db "Enter a Uppercase Character: $"
+temp2 db "Lowercase Character is: "
+temp3 db "?","$"
+
+
+;Code Segment
+.Code 
+ 
+;Move Data Segment
+MOV AX, @data
+MOV DS, AX         
+
+
+;Printing String
+MOV AH,9                       
+LEA DX,temp1
+INT 21h                     
+
+
+;Taking Input
+MOV AH,1
+INT 21h
+
+;Converting Lowercase & Copy to temp3
+ADD AL,20h   
+MOV temp3,AL
+
+
+;Move cursor at the begining of the line
+MOV DL,0Dh
+MOV AH,2
+INT 21h
+
+;Move cursor to the new line
+MOV DL,0Ah
+MOV AH,2
+INT 21h  
+
+;Printing the string with result
+LEA DX,temp2
+MOV AH,9
+INT 21h      
